f092b59765e44609b1c58c58db2fdbf65a10cfd0
Nathan Roberts <superzero@debian-BULLSEYE-live-builder-AMD64>
2022-09-12 21:32:00 -0400

    First challenge

---
+++ 01.py
@@ -0,0 +1,17 @@
+from base64 import b16decode, b64encode
+
+
+def hex__to_b64(data_hex: bytes) -> bytes:
+	return b64encode(b16decode(data_hex, casefold=True))
+
+
+if __name__ == "__main__":
+	data_hex=b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	data_raw=b16decode(data_hex, casefold=True)
+	data_b64=b64encode(data_raw)
+
+	print(f"{data_hex=}")
+	print(f"{data_raw=}")
+	print(f"{data_b64=}")
+	if data_b64 == b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t":
+		print("It worked!")
