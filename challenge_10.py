fe88d8edffe6715b5ac9699a2fc2ff5077b8554f
Nathan Roberts <natedagrape2@gmail.com>
2022-09-23 17:26:53 -0400

    Updated operation to work with new ecb and padding functions

---
+++ challenge_10.py
@@ -0,0 +1,45 @@
+from challenge_07 import encrypt_AES_128_ECB, decrypt_AES_128_ECB
+from challenge_02 import xor_bytes
+from challenge_09 import pkcs7_padding, pkcs7_strip
+from base64 import b64decode
+
+
+def encrypt_aes_cbc(key: bytes, pt: bytes, iv: bytes) -> bytes:
+    key_len = len(key)
+    local_pt = pkcs7_padding(pt, key_len)
+
+    num_of_blocks = len(local_pt) // key_len
+    pt_blocks = [local_pt[inc: inc + key_len] for inc in range(0, num_of_blocks, key_len)]
+    ct_blocks = []
+    for i, block in enumerate(pt_blocks):
+        if i == 0:
+            prev_block = iv
+        else:
+            prev_block = ct_blocks[-1]
+        ct_blocks.append(encrypt_AES_128_ECB(key, xor_bytes(block, prev_block)))
+    return b''.join(ct_blocks)
+
+
+def decrypt_aes_cbc(key: bytes, ct: bytes, iv: bytes) -> bytes:
+    key_len = len(key)
+    num_of_blocks = len(ct) // key_len
+    ct_blocks = [ct[inc: inc + key_len] for inc in range(0, num_of_blocks, key_len)]
+    pt_blocks = []
+    for i, block in reversed(list(enumerate(ct_blocks))):
+        if i == 0:
+            prev_block = iv
+        else:
+            prev_block = ct_blocks[i - 1]
+        pt_blocks.append(xor_bytes(decrypt_AES_128_ECB(key, block), prev_block))
+    print(b''.join(reversed(pt_blocks)))
+
+    return pkcs7_strip(b''.join(reversed(pt_blocks)))
+
+
+if __name__ == "__main__":
+    with open("challenge_10_text.txt", "rb") as f:
+        b64_ct = f.read()
+    ct = b64decode(b64_ct)
+    key = b'YELLOW SUBMARINE'
+    iv = b'\x00' * len(key)
+    print(decrypt_aes_cbc(key, ct, iv))
